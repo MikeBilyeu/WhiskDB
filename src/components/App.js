ba5f76b3e13b0e8d50c57ae13baa3c79df8e261a
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-01-27 13:03:28 -0500

    axios api request

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 import edamam from '../api/edamam';
 import Header from './Header';
 import ContentContainer from './ContentContainer';
 import SearchBar from './SearchBar';
 
+require('dotenv').config();
+
 class App extends React.Component {
   state = { recipes: [] };
 
   onSearchSubmit(term) {
-    console.log("This is the Search Term: ", term);
+    axios.get('https://api.edamam.com/search', {
+      params: {
+        q: term,
+        app_id: process.env.REACT_APP_APP_ID,
+        app_key: process.env.REACT_APP_APP_KEY
+      }
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   }
 
   render() {
