c2deb7c704801cc96652b7ddfe65dd2c0f25a4b8
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-01-28 15:48:08 -0500

    update recipe schema ready to reacive recipe title

--- backend/data.js
+++ backend/data.js
@@ -3,13 +3,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // this will be our data base's data structure
-const DataSchema = new Schema(
+const recipeSchema = new Schema(
   {
-    id: Number,
-    message: String
-  },
-  { timestamps: true }
+    title: String
+  }
 );
 
 // export the new Schema so we could modify it using Node.js
-module.exports = mongoose.model("Data", DataSchema);
+module.exports = mongoose.model("Data", recipeSchema);
