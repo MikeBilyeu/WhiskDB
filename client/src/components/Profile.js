89f890d01c58aa139ee04e13a43d7987c5611518
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-01-29 14:21:40 -0500

    Recipe button onclick renders create recipe components

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,13 +1,33 @@
 import React from 'react';
 
-const Profile = (props) => {
-  return (
-    <div>
-      <h1>Profile</h1>
-      <h2>{props.userName}</h2>
-      <button>Create Recipe</button>
-    </div>
-  );
+class Profile extends React.Component {
+  state = { profileButton: '' }
+
+  onButtonClick = (e) => {
+    console.log(e);
+    this.setState({ profileButton: 'create' });
+  }
+
+  render() {
+    if(!this.state.profileButton) {
+      return (
+        <div>
+          <h1>Profile</h1>
+          <h2>{this.props.userName}</h2>
+          <button onClick={this.onButtonClick}>Create Recipe</button>
+        </div>
+      );
+    } else if(this.state.profileButton === 'create') {
+      return (
+        <div>
+          <h1>Create Recipe</h1>
+          <input type="text" placeholder="Recipe Title" />
+          <button>Done</button>
+        </div>
+      );
+    }
+
+  }
 }
 
 export default Profile;
