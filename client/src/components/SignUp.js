46ed19dd18b3866e831c4d74d4dcb3a0cb3163ab
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-02-01 13:15:41 -0500

    State added for username and password sign up form

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,23 +1,47 @@
 import React from 'react';
 
-const SignUp = () => {
-  return (
-    <form className="ui form">
-      <h2 className="ui center aligned icon header">
-        <i className="user circle icon"></i>
-        Sign up
-      </h2>
-      <div className="field">
-        <label>Username</label>
-        <input type="text" name="username" placeholder="Username" />
-      </div>
-      <div className="field">
-        <label>Password</label>
-        <input type="Password" name="Password" placeholder="Password" />
-      </div>
-      <button className="ui button blue" type="submit">Sign up</button>
-    </form>
-  );
+class SignUp extends React.Component {
+  state = { username: '', password: '' }
+
+  onUsernameChange = (event) => {
+    this.setState({ username: event.target.value});
+  }
+
+  onPasswordChange = (event) => {
+    this.setState({ password: event.target.value});
+  }
+
+  render() {
+    return (
+      <form className="ui form">
+        <h2 className="ui center aligned icon header">
+          <i className="user circle icon"></i>
+          Sign up
+        </h2>
+        <div className="field">
+          <label>Username</label>
+          <input
+            value={this.state.username}
+            onChange={this.onUsernameChange}
+            type="text"
+            name="username"
+            placeholder="Username"
+          />
+        </div>
+        <div className="field">
+          <label>Password</label>
+          <input
+            value={this.state.password}
+            onChange={this.onPasswordChange}
+            type="Password"
+            name="Password"
+            placeholder="Password"
+          />
+        </div>
+        <button className="ui button blue" type="submit">Sign up</button>
+      </form>
+    );
+  }
 }
 
 export default SignUp;
