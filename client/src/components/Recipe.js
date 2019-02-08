ace309982dc7770e46766dbac9b6e1d84f1d3fe0
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-02-08 16:04:41 -0500

    Add to recipe component

--- client/src/components/Recipe.js
+++ client/src/components/Recipe.js
@@ -1,68 +1,67 @@
 import React from "react";
 import Button from "./Button";
 
-const Recipe = props => {
-  return (
-    <div>
-      <div>Stars</div>
-      <div>num of votes</div>
-      <h1>Title of Recipe</h1>
-      <h4>-Author of Recipe</h4>
-      <div>Time</div>
-      <img src="" alt="Recipe image" />
-      <Button text="Change Yeild" />
-      <p>Servings</p>
-      <h2>Ingredients</h2>
-      <ul>
-        <li>Ingredient</li>
-        <li>Ingredient</li>
-        <li>Ingredient</li>
-        <li>Ingredient</li>
-      </ul>
-      <h2>Directions</h2>
-      <ol>
-        <li>
-          <p>
-            It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout. The point
-            of using Lorem Ipsum is that it has a more-or-less normal
-            distribution of letters, as opposed to using 'Content here, content
-            here', making it look like readable English. Many desktop publishing
-            packages and web page editors now use Lorem Ipsum as their default
-            model text, and a search for 'lorem ipsum' will uncover many web
-            sites still in their infancy.
-          </p>
+class Recipe extends React.Component {
+  state = {
+    title: "Authenic Tacos",
+    votes: 115,
+    author: "Mike Bilyeu",
+    time: "35m",
+    imageURL:
+      "https://images.unsplash.com/photo-1545092844-0c99aa3b6cab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
+    servings: 3,
+    ingredients: ["1 Cup Flour", "1 tbs Water"],
+    directions: [
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy.",
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy.",
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy."
+    ]
+  };
+
+  renderIngredients = ingredients => {
+    return ingredients.map(ingredient => {
+      return <li key={ingredient}>{ingredient}</li>;
+    });
+  };
+
+  renderDirections = directions => {
+    return directions.map((step, i) => {
+      return (
+        <li key={i}>
+          <p>{step}</p>
         </li>
-        <li>
-          <p>
-            It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout. The point
-            of using Lorem Ipsum is that it has a more-or-less normal
-            distribution of letters, as opposed to using 'Content here, content
-            here', making it look like readable English. Many desktop publishing
-            packages and web page editors now use Lorem Ipsum as their default
-            model text, and a search for 'lorem ipsum' will uncover many web
-            sites still in their infancy.
-          </p>
-        </li>
-        <li>
-          <p>
-            It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout. The point
-            of using Lorem Ipsum is that it has a more-or-less normal
-            distribution of letters, as opposed to using 'Content here, content
-            here', making it look like readable English. Many desktop publishing
-            packages and web page editors now use Lorem Ipsum as their default
-            model text, and a search for 'lorem ipsum' will uncover many web
-            sites still in their infancy.
-          </p>
-        </li>
-      </ol>
-      <h2>How was it?</h2>
-      <div>Thumbs up</div>
-      <div>Thumbs down</div>
-    </div>
-  );
-};
+      );
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <i className="star icon" />
+        <i className="star icon" />
+        <i className="star icon" />
+        <i className="star icon" />
+        <i className="star outline icon" />
+        <div>{this.state.votes} votes</div>
+        <h1>{this.state.title}</h1>
+        <h4>-{this.state.author}</h4>
+        <div>Time: {this.state.time}</div>
+        <img
+          className="ui medium bordered image big"
+          src={this.state.imageURL}
+        />
+        <Button text="Change Yeild" />
+        <p>Servings: {this.state.servings}</p>
+        <h2>Ingredients</h2>
+        <ul>{this.renderIngredients(this.state.ingredients)}</ul>
+        <h2>Directions</h2>
+        <ol>{this.renderDirections(this.state.directions)}</ol>
+        <h2>How was it?</h2>
+        <i className="thumbs up icon big" />
+        <i className="thumbs down icon big" />
+      </div>
+    );
+  }
+}
 
 export default Recipe;
