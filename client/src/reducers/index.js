89808fca8e4e010065abc2a1581d67af8e726166
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-02-21 20:05:46 -0500

    actions and reducers set up

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
+import authReducer from "./authReducer";
+import errorReducer from "./errorReducer";
 
 export default combineReducers({
-  form: formReducer
+  form: formReducer,
+  auth: authReducer,
+  errors: errorReducer
 });
