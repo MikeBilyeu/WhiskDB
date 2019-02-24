2af450bc668fd3281e723638717f764e0ec7bd98
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-02-24 17:20:35 -0500

    remove unnecessary files

--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,3 @@
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const logger = require("morgan");
-// const passport = require("passport");
-//
-// const users = require("./routes/api/users");
-//
-// const dbConnection = require("./database");
-// // const User = require('./database/models/user');
-//
-// const PORT = process.env.PORT || 3001;
-// const app = express();
-//
-// // Parses the request body to be a readable json format
-// // Passport middleware
-// // app.use(passport.initialize());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-// app.use(logger("dev"));
-//
-// // Passport config
-// // require("./config/passport")(passport);
-//
-// // Routes
-// app.get("/", (request, response) => {
-//   response.json({ info: "Node.js, Express, and Postgres API" });
-// });
-//
-// // Routes
-// // app.use("/api/users", users);
-//
-// // // Sign up
-// // app.use("/sign-up", require("./routes/user"));
-// //
-// // //Create Recipe
-// // app.use("/create-recipe", require("./routes/recipe"));
-//
-// // launch our backend into a port
-// app.listen(PORT, () => console.log(`LISTENING ON PORT ${PORT}`));
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -56,10 +16,6 @@ app.use(
 app.use(passport.initialize());
 // Passport config
 require("./config/passport")(passport);
-
-app.get("/hello", (request, response) => {
-  response.json({ info: "Node.js, Express, and Postgres API" });
-});
 
 app.post("/register", db.createUser);
 
