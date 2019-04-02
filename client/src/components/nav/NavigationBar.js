a84a05d10ba5e396961f5b759e52dcb0bc961686
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-04-02 16:15:11 -0400

    change nav bar

--- client/src/components/nav/NavigationBar.js
+++ client/src/components/nav/NavigationBar.js
@@ -1,22 +1,13 @@
 import React from "react";
 import NavigationButton from "./NavigationButton";
-import { withRouter } from "react-router";
 
 const NavigationBar = props => {
   return (
     <div className="ui inverted fluid three borderless item bottom fixed menu">
-      <a
-        className="item"
-        onClick={() => props.history.goBack()}
-        style={{ border: "none", cursor: "pointer" }}
-      >
-        <i className="icon large arrow left" />
-        Back
-      </a>
-      <NavigationButton link="/" icon="home" text="Home" />
-      <NavigationButton link="/profile" icon="user" text="Profile" />
+      <NavigationButton link="/" icon="home" />
+      <NavigationButton link="/profile/saved-recipes" icon="book" />
+      <NavigationButton link="/profile" icon="user" />
     </div>
   );
 };
-
-export default withRouter(NavigationBar);
+export default NavigationBar;
