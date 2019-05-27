7517101da9807e068fbb04d3512a822db2e7b616
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-05-27 15:48:15 -0400

    query diet and cuisine

--- backend/queries/getBrowseRecipes.js
+++ backend/queries/getBrowseRecipes.js
@@ -15,6 +15,10 @@ const getBrowseRecipes = (request, response) => {
   let { search, meal, diet, cuisine, sort } = JSON.parse(
     request.query.browseData.toLowerCase()
   );
+  diet = diet === "none" ? null : diet;
+  cuisine = cuisine === "all" ? null : cuisine;
+
+  const numOfCats = !diet && !cuisine ? 1 : !diet || !cuisine ? 2 : 3;
 
   //connect pool
   pool.connect().then(client => {
@@ -25,12 +29,39 @@ const getBrowseRecipes = (request, response) => {
         count(lr.*) AS likes, count(dr.*) AS dislikes FROM recipes r LEFT JOIN
         liked_recipes lr ON r.recipe_id = lr.recipe_liked LEFT JOIN
         disliked_recipes dr ON r.recipe_id = dr.recipe_disliked WHERE
-        r.recipe_id IN
-        ( SELECT recipe FROM recipes_join_categories WHERE
-          category in (SELECT category_id from categories
-            WHERE category_name = $1))
-        GROUP BY r.recipe_id`,
-        [meal]
+        r.recipe_id in
+        ( SELECT recipe FROM recipes_join_categories
+          WHERE
+          CASE
+          WHEN $2 != '' AND $3 != '' THEN
+            category = (SELECT category_id FROM categories
+            WHERE category_name = $1)
+            OR
+            category = (SELECT category_id
+            FROM categories WHERE category_name = $2)
+            OR
+            category = (SELECT category_id
+            FROM categories WHERE category_name = $3)
+          WHEN $2 != '' THEN
+            category = (SELECT category_id FROM categories
+            WHERE category_name = $1)
+            OR
+            category = (SELECT category_id
+            FROM categories WHERE category_name = $2)
+          WHEN $3 != '' THEN
+            category = (SELECT category_id FROM categories
+            WHERE category_name = $1)
+            OR
+            category = (SELECT category_id
+            FROM categories WHERE category_name = $3)
+          ELSE
+            category = (SELECT category_id FROM categories
+            WHERE category_name = $1)
+          END
+          GROUP BY recipe HAVING COUNT(*) = $4 )
+        GROUP BY r.recipe_id
+`,
+        [meal, diet, cuisine, numOfCats]
       )
       .then(res => {
         client.release();
