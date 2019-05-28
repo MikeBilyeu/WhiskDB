dde0017b2c3f270abf3067f6cc117289913e931a
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-05-28 15:59:34 -0400

    starColor is stored in state

--- client/src/components/recipes/recipe-display/rating/Rating.js
+++ client/src/components/recipes/recipe-display/rating/Rating.js
@@ -2,27 +2,35 @@ import React from "react";
 
 import { ReactComponent as Star } from "./star.svg";
 
-const Rating = ({ votes, rating }) => {
-  const renderRating = () => {
-    let starColor = [];
-    // push color of star to arr
+class Rating extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { starColor: [] };
+  }
+  componentDidMount() {
     for (let i = 0; i < 5; i++) {
-      if (i < rating || rating < 1) {
-        starColor = [...starColor, "#FFBB5F"];
-      } else {
-        starColor = [...starColor, "#E2E2E2"];
-      }
+      this.setState(({ starColor }, { rating }) => {
+        return {
+          starColor: [...starColor, rating > i ? "#FFBB5F" : "#E2E2E2"]
+        };
+      });
     }
-    return starColor.map((color, i) => {
+  }
+  renderRating = () => {
+    return this.state.starColor.map((color, i) => {
       return <Star key={"star" + i} style={{ width: "1.3rem", fill: color }} />;
     });
   };
-  return (
-    <div style={{ display: "grid", gridTemplateColumns: "7rem 1fr" }}>
-      <div>{renderRating()}</div>
-      <div>{`${votes} vote${votes !== 1 ? "s" : ""}`}</div>
-    </div>
-  );
-};
+
+  render() {
+    const { votes } = this.props;
+    return (
+      <div style={{ display: "grid", gridTemplateColumns: "7rem 1fr" }}>
+        <div>{this.renderRating()}</div>
+        <div>{`${votes} vote${votes !== 1 ? "s" : ""}`}</div>
+      </div>
+    );
+  }
+}
 
 export default Rating;
