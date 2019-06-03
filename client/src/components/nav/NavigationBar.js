e7b0d45c00f7008074469a83f29121b7ea1f06da
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-06-03 13:14:09 -0400

    style nav icons from state

--- client/src/components/nav/NavigationBar.js
+++ client/src/components/nav/NavigationBar.js
@@ -1,26 +1,72 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import "./nav-styles.css";
 
 import { ReactComponent as Home } from "./search.svg";
 import { ReactComponent as Saved } from "./saved.svg";
 import { ReactComponent as Profile } from "./profile.svg";
 
-const NavigationBar = props => {
-  return (
-    <div className="navBar">
-      <Link to="/profile/saved-recipes">
-        <Saved className="navIcon" />
-      </Link>
+//change to class based ReactComponent
 
-      <Link to="/">
-        <Home className="navIcon" />
-      </Link>
+class NavigationBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nav: ""
+    };
+  }
+  componentDidMount() {
+    this.setNavState();
+  }
+  // set state depending on url location
+  componentWillReceiveProps(nextProps) {
+    // prevent multi clicks
+    if (this.props.location.pathname !== nextProps.location.pathname) {
+      this.setNavState();
+    }
+  }
 
-      <Link to="/profile">
-        <Profile className="navIcon" />
-      </Link>
-    </div>
-  );
-};
-export default NavigationBar;
+  setNavState = () => {
+    this.setState((prevState, props) => {
+      const pathname = props.location.pathname;
+      if (/saved-recipes/.test(pathname)) {
+        return { nav: "saved" };
+      } else if (/profile|auth/.test(pathname)) {
+        return { nav: "profile" };
+      } else {
+        return { nav: "home" };
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div className="navBar">
+        <Link to="/profile/saved-recipes">
+          <Saved
+            className={
+              "navIcon" + (this.state.nav === "saved" ? " navActive" : "")
+            }
+          />
+        </Link>
+
+        <Link to="/">
+          <Home
+            className={
+              "navIcon" + (this.state.nav === "home" ? " navActive" : "")
+            }
+          />
+        </Link>
+
+        <Link to="/profile">
+          <Profile
+            className={
+              "navIcon" + (this.state.nav === "profile" ? " navActive" : "")
+            }
+          />
+        </Link>
+      </div>
+    );
+  }
+}
+export default withRouter(NavigationBar);
