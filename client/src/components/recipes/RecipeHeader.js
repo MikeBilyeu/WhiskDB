ea7562983828e4d4e70bd5b18cda3ff18d2c9caf
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-06-05 14:44:22 -0400

    add styles for recipe header

--- client/src/components/recipes/RecipeHeader.js
+++ client/src/components/recipes/RecipeHeader.js
@@ -1,26 +1,35 @@
 import React from "react";
-
 import { connect } from "react-redux";
-
 import { withRouter } from "react-router-dom";
 
+import { ReactComponent as Arrow } from "./arrowLeft.svg";
+import { ReactComponent as Check } from "./check.svg";
+import { ReactComponent as Share } from "./share.svg";
+
+//styles
+import "./recipe-styles.css";
 // import action creator
 import { saveRecipe } from "../../actions/recipeActions";
 
 const RecipeHeader = props => {
   const { recipe_id, user_id, saveRecipe, recipeSaved, history } = props;
   return (
-    <div>
-      <button onClick={() => history.goBack()}>Back</button>
-      <button
+    <div className="recipe-header">
+      <Arrow className="back-btn" onClick={() => history.goBack()} />
+
+      <div
+        className={
+          "save-btn" + (recipeSaved && user_id !== null ? " saved" : "")
+        }
         onClick={() => saveRecipe(recipe_id, user_id)}
-        style={{
-          color: recipeSaved && user_id !== null ? "Green" : "black"
-        }}
       >
-        Save
-      </button>
-      <button>Share</button>
+        {recipeSaved && user_id !== null ? "Saved" : "Save"}
+        {recipeSaved && user_id !== null ? (
+          <Check style={{ width: "1rem", marginLeft: ".2rem" }} />
+        ) : null}
+      </div>
+
+      <Share className="share-btn" />
     </div>
   );
 };
