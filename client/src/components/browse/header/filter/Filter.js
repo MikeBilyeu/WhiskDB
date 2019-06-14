cd503b13260857d8c0a14efb84bccf8e1505d14c
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-06-13 20:57:20 -0400

    use overflow x scroll

--- client/src/components/browse/header/filter/Filter.js
+++ client/src/components/browse/header/filter/Filter.js
@@ -7,13 +7,6 @@ import "./filter-styles.css";
 class Filter extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      xAxis: 0,
-      touchStart: 0,
-      touchMove: 0,
-      startMoveDiff: 0
-    };
   }
   renderFilterOptions = () => {
     return this.props.filterOptions.map((option, i) => {
@@ -26,61 +19,16 @@ class Filter extends React.Component {
       );
     });
   };
-  handleTouchStart = e => {
-    e.persist();
-    this.setState({
-      touchStart: e.touches[0].clientX,
-      touchMove: e.touches[0].clientX
-    });
-  };
 
-  handleTouchMove = e => {
-    e.persist();
-    this.setState(prevState => {
-      const diff = e.touches[0].clientX - prevState.touchMove;
-      return {
-        startMoveDiff: e.touches[0].clientX - prevState.touchStart,
-        touchMove: e.touches[0].clientX,
-        xAxis:
-          prevState.xAxis + diff > 0
-            ? 0
-            : prevState.xAxis + diff < -this.barDiff
-            ? -this.barDiff
-            : prevState.xAxis + diff
-      };
-    });
-  };
-  barWidth = 0;
-  outerBarWidth = 0;
-  barDiff = 0;
   render() {
-    this.barDiff = Math.abs(this.barWidth - this.outerBarWidth);
-
     return (
       <div
         className="outer-bar"
         onTouchStart={this.handleTouchStart}
         onTouchMove={this.handleTouchMove}
         onTouchEnd={this.handleTouchEnd}
-        ref={el => {
-          if (el) {
-            this.outerBarWidth = el.offsetWidth;
-          }
-        }}
       >
-        <div
-          className="filter-bar"
-          style={{
-            left: this.state.xAxis
-          }}
-          ref={el => {
-            if (el) {
-              this.barWidth = el.offsetWidth;
-            }
-          }}
-        >
-          {this.renderFilterOptions()}
-        </div>
+        <div className="filter-bar">{this.renderFilterOptions()}</div>
       </div>
     );
   }
