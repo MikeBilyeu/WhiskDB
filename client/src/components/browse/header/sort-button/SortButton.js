3e61a1cdebbf8ac1dfe60638ec43feb8f493487a
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-06-24 15:30:01 -0400

    sort button working

--- client/src/components/browse/header/sort-button/SortButton.js
+++ client/src/components/browse/header/sort-button/SortButton.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { connect } from "react-redux";
+
+// diet action creator
+import { toggleFilterButton } from "../../../../actions/browseActions";
+
 import { ReactComponent as SortIcon } from "./SortIcon.svg";
-const SortButton = () => {
-  return <SortIcon className="sort-icon" />;
+
+const SortButton = props => {
+  const handleClick = () => {
+    props.toggleFilterButton("Sort");
+  };
+  return (
+    <SortIcon
+      style={{ fill: props.buttonToggled === "Sort" ? "#0172C4" : "#676767" }}
+      onClick={handleClick}
+      className="sort-icon"
+    />
+  );
 };
 
-export default SortButton;
+const mapSateToProps = state => {
+  return {
+    buttonToggled: state.browseRecipes.toggleFilterButton
+  };
+};
+
+export default connect(
+  mapSateToProps,
+  { toggleFilterButton }
+)(SortButton);
