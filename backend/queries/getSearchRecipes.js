e984b9aa8dc227c340cf340d34fd18f5d3047bc4
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-07-23 13:47:16 -0400

    Get username in query on getSearchRecipe

--- backend/queries/getSearchRecipes.js
+++ backend/queries/getSearchRecipes.js
@@ -18,15 +18,17 @@ const getSearchRecipes = (request, response) => {
     // query the recipe table
     return client
       .query(
-        `SELECT r.recipe_id, r.title, r.total_time_mins, r.image_url,
+        `SELECT r.recipe_id, u.username AS username, r.title, r.total_time_mins, r.image_url,
         r.created_at, CASE WHEN COUNT(lr.*) + COUNT(dr.*) = 0 THEN 0
         ELSE (count(lr.*) / CAST (COUNT(lr.*) + COUNT(dr.*) AS FLOAT)) * 5
         END
         AS rating,
-        CAST(COUNT(lr.*) + COUNT(dr.*) AS INTEGER) AS votes FROM recipes r LEFT JOIN
-        liked_recipes lr ON r.recipe_id = lr.recipe_liked LEFT JOIN
-        disliked_recipes dr ON r.recipe_id = dr.recipe_disliked WHERE document_vectors @@ to_tsquery($1)
-        GROUP BY r.recipe_id
+        CAST(COUNT(lr.*) + COUNT(dr.*) AS INTEGER) AS votes FROM recipes r
+        LEFT JOIN users u ON r.created_by = u.user_id
+        LEFT JOIN liked_recipes lr ON r.recipe_id = lr.recipe_liked
+        LEFT JOIN disliked_recipes dr ON r.recipe_id = dr.recipe_disliked
+        WHERE document_vectors @@ to_tsquery($1)
+        GROUP BY r.recipe_id, u.user_id
         ORDER BY rating DESC, votes DESC;
 `,
         [search]
