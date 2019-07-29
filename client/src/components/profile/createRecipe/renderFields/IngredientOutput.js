8e212d6296879a3714b4fefb86d615e57d6403e8
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-07-29 15:34:52 -0400

    Ingredient input returns fields array values but not last field values

--- client/src/components/profile/createRecipe/renderFields/IngredientOutput.js
+++ client/src/components/profile/createRecipe/renderFields/IngredientOutput.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { connect } from "react-redux";
+import { formValueSelector } from "redux-form"; // ES6
+const selector = formValueSelector("newRecipe");
 
-const IngredientOutput = () => {
-  return <div>Ingredient Output</div>;
+const IngredientOutput = ingredients => {
+  return (
+    <div>
+      {ingredients.ingredients.map((ingredient, i, arr) => {
+        // don't render last field values must click add button to add
+        if (i !== arr.length - 1) {
+          return (
+            <div key={ingredient.ingredient + i}>{`${
+              ingredient.amount
+            } ${ingredient.unit || ""} ${ingredient.ingredient}
+              ${ingredient.prep ? `(${ingredient.prep})` : ""}`}</div>
+          );
+        }
+        return null;
+      })}
+    </div>
+  );
 };
 
-export default IngredientOutput;
+const mapSateToProps = state => {
+  return { ingredients: selector(state, "ingredients") };
+};
+
+export default connect(mapSateToProps)(IngredientOutput);
