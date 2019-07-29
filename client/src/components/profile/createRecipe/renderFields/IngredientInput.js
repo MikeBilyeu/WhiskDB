160f900a14b65531f667e21ebbbe170381304332
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-07-29 14:29:46 -0400

    Ingredients fieldarry only displays the last field but form status next button needs to validate while ingredient field is empty

--- client/src/components/profile/createRecipe/renderFields/IngredientInput.js
+++ client/src/components/profile/createRecipe/renderFields/IngredientInput.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { Field } from "redux-form";
+import { getFormSyncErrors } from "redux-form";
+import { connect } from "react-redux";
 
 import UnitDropDown from "../inputs/UnitDropDown";
 import TextInput from "../inputs/TextInput";
 
 // Renders all of the ingredient fields i.e. ingredient, unit, amount
-const IngredientInput = ({ fields = {}, meta: { touched, error } }) => {
+const IngredientInput = ({
+  fields = {},
+  meta: { touched, error },
+  syncErrors
+}) => {
   const capitalize = value => {
     return value && value.charAt(0).toUpperCase() + value.substring(1);
   };
@@ -13,51 +19,54 @@ const IngredientInput = ({ fields = {}, meta: { touched, error } }) => {
     let strArr = value.match(/.{0,55}/) || [""];
     return value && strArr[0];
   };
-
   return (
     <div>
-      {fields.map((ingredient, index) => (
-        <div key={index}>
-          <div className="fields ingredients">
-            <Field
-              name={`${ingredient}.ingredient`}
-              component={TextInput}
-              label={`Ingredient`}
-              addClass="full-input ingredient"
-              placeholder="E.g. Red Bell Pepper"
-              normalize={capitalize}
-              parse={textParse}
-            />
-            <Field
-              name={`${ingredient}.amount`}
-              component={TextInput}
-              label="Amount"
-              placeholder="1 1/2"
-              addClass="amount"
-            />
-            <Field
-              addClass="unit"
-              name={`${ingredient}.unit`}
-              component={UnitDropDown}
-            />
-            <Field
-              name={`${ingredient}.prep`}
-              component={TextInput}
-              label="Prep"
-              addClass="full-input prep"
-              placeholder="Diced"
-              parse={textParse}
-            />
-          </div>
-        </div>
-      ))}
-
+      {fields.map((ingredient, index, arr) => {
+        //only display last field in the fieldarray
+        if (index === arr.length - 1) {
+          return (
+            <div key={index}>
+              <div className="fields ingredients">
+                <Field
+                  name={`${ingredient}.ingredient`}
+                  component={TextInput}
+                  label={`Ingredient`}
+                  addClass="full-input ingredient"
+                  placeholder="E.g. Red Bell Pepper"
+                  normalize={capitalize}
+                  parse={textParse}
+                />
+                <Field
+                  name={`${ingredient}.amount`}
+                  component={TextInput}
+                  label="Amount"
+                  placeholder="1 1/2"
+                  addClass="amount"
+                />
+                <Field
+                  addClass="unit"
+                  name={`${ingredient}.unit`}
+                  component={UnitDropDown}
+                />
+                <Field
+                  name={`${ingredient}.prep`}
+                  component={TextInput}
+                  label="Prep"
+                  addClass="full-input prep"
+                  placeholder="Diced"
+                  parse={textParse}
+                />
+              </div>
+            </div>
+          );
+        }
+      })}
       <div className="add-remove-button">
         <div
           className="button add"
           type="button"
           onClick={() => {
-            if (fields.length < 20) {
+            if (Object.keys(syncErrors).length < 1) {
               fields.push({});
             }
           }}
@@ -69,4 +78,8 @@ const IngredientInput = ({ fields = {}, meta: { touched, error } }) => {
   );
 };
 
-export default IngredientInput;
+const mapSateToProps = state => {
+  return { syncErrors: getFormSyncErrors("newRecipe")(state) };
+};
+
+export default connect(mapSateToProps)(IngredientInput);
