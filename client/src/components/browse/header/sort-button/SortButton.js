20c3c4e2ba7089e4b948e6e4e195c248c0afb08a
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-08-07 15:02:46 -0400

    Add sort text to bottom of sort icon fix padding issues with whiskDB title

--- client/src/components/browse/header/sort-button/SortButton.js
+++ client/src/components/browse/header/sort-button/SortButton.js
@@ -11,17 +11,37 @@ const SortButton = props => {
     props.toggleFilterButton("Sort");
   };
   return (
-    <SortIcon
-      style={{ fill: props.buttonToggled === "Sort" ? "#0172C4" : "#676767" }}
+    <div
       onClick={handleClick}
-      className="sort-icon"
-    />
+      style={{
+        gridArea: "sort",
+        justifySelf: "start",
+        display: "grid",
+        cursor: "pointer",
+        width: "2.5rem",
+        placeItems: "center",
+        paddingLeft: "0.3rem"
+      }}
+    >
+      <SortIcon
+        style={{ fill: props.buttonToggled === "Sort" ? "#0172C4" : "#676767" }}
+        className="sort-icon"
+      />
+      <div style={{ fontSize: ".7rem", color: "#B7B7B7" }}>
+        {props.sort === "Top Rated"
+          ? "Rated"
+          : props.sort === "Newest"
+          ? "New"
+          : props.sort}
+      </div>
+    </div>
   );
 };
 
 const mapSateToProps = state => {
   return {
-    buttonToggled: state.browseRecipes.toggleFilterButton
+    buttonToggled: state.browseRecipes.toggleFilterButton,
+    sort: state.browseRecipes.browseData.sort
   };
 };
 
