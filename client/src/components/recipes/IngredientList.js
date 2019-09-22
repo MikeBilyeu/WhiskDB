9ec3ae1c84d57fbcb053aa1e65d47e08f25ed850
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-09-22 13:40:02 -0400

    Add line through ingredient onclcik

--- client/src/components/recipes/IngredientList.js
+++ client/src/components/recipes/IngredientList.js
@@ -5,33 +5,73 @@ import ServingsAndUnit from "./servingsAndUnit/ServingsAndUnit";
 //selector
 import convertIngredients from "../../selectors/ingredientsSelector";
 
-const renderIngredientList = props => {
-  return props.ingredients.map(({ amount, unit = "", ingredient, prep }, i) => {
-    prep = prep ? `(${prep})` : "";
+class Ingredient extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isStrikeThrough: false
+    };
+  }
+
+  handleClick = () => {
+    this.setState(({ isStrikeThrough }) => {
+      return { isStrikeThrough: !isStrikeThrough };
+    });
+  };
+  render() {
+    const { amount, unit, ingredient, prep } = this.props;
+    // map over amounts if multiple amounts e.g. 1 cup + 1/2 cup
     if (typeof amount === "object") {
       let amounts = amount.map(amount => {
         return `${amount.amount} ${amount.unit}`;
       });
       return (
-        <li key={`ingredient${i}`}>{`${amounts.join(
-          " + "
-        )} ${ingredient} ${prep}`}</li>
+        <li
+          onClick={this.handleClick}
+          style={{
+            textDecoration: this.state.isStrikeThrough
+              ? "line-through"
+              : "none",
+            opacity: this.state.isStrikeThrough ? ".2" : "1"
+          }}
+        >{`${amounts.join(" + ")} ${ingredient} ${prep}`}</li>
       );
     }
+
     return (
-      <li key={`ingredient${i}`}>
+      <li
+        onClick={this.handleClick}
+        style={{
+          textDecoration: this.state.isStrikeThrough ? "line-through" : "none",
+          opacity: this.state.isStrikeThrough ? ".2" : "1"
+        }}
+      >
         {`${amount} ${unit} ${ingredient} ${prep}`}
       </li>
     );
-  });
-};
+  }
+}
 
 const IngredientList = props => {
   return (
     <div className="recipe-list ingredients">
       <h2>Ingredients</h2>
       <ServingsAndUnit />
-      <ul>{renderIngredientList(props)}</ul>
+      <ul>
+        {props.ingredients.map(({ amount, unit = "", ingredient, prep }, i) => {
+          prep = prep ? `(${prep})` : "";
+          return (
+            <Ingredient
+              key={`ingredient${i}`}
+              amount={amount}
+              unit={unit}
+              ingredient={ingredient}
+              prep={prep}
+              i={i}
+            />
+          );
+        })}
+      </ul>
     </div>
   );
 };
