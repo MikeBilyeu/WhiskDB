e8002cb44faa18189d088960e996620f3a9f9426
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-10-28 12:46:54 -0400

    Replace FilterButton component with generic Button component

--- client/src/components/browse/header/HomeHeader.js
+++ client/src/components/browse/header/HomeHeader.js
@@ -1,37 +1,73 @@
 import React from "react";
 import { connect } from "react-redux";
 
-// diet action creator
+// actions
 import { toggleFilterButton } from "../../../actions/browseActions";
 
+// components
 import SearchBar from "./search-bar/SearchBar";
 import Whiskdb from "../../../images/whiskdb.png";
 import { SortButton } from "../../sort-button/SortButton";
-import FilterButton from "./filter-buttons/FilterButton";
+import { Button } from "../../Button";
+import { ReactComponent as Arrow } from "../../../images/filterArrow.svg";
 
 //styles
 import "./header-styles.css";
 
-const HomeHeader = ({ sortBy, buttonToggled, toggleFilterButton }) => {
+const HomeHeader = ({
+  sortBy,
+  diet,
+  meal,
+  activeFilterBtn,
+  toggleFilterButton
+}) => {
+  const dietBtnActive = activeFilterBtn === "Diet";
+  const mealBtnActive = activeFilterBtn === "Meal";
+  const sortBtnActive = activeFilterBtn === "Sort";
+
+  const dietBtnStyle = `filter-btn diet ${dietBtnActive &&
+    "filter-active"} ${diet !== "None" && "filter-select"}`;
+  const dietArrowStyle = `filter-arrow ${dietBtnActive && "filter-active"}`;
+
+  const mealBtnStyle = `filter-btn meal ${mealBtnActive &&
+    "filter-active"} ${meal !== "All Meals" && "filter-select"}`;
+  const mealArrowStyle = `filter-arrow ${mealBtnActive && "filter-active"}`;
+
   return (
     <div className="header">
       <SearchBar />
       <img className="whisk-title" src={Whiskdb} alt="Whiskdb logo" />
       <SortButton
         onClick={() => toggleFilterButton("Sort")}
-        sortActive={buttonToggled === "Sort"}
+        sortActive={sortBtnActive}
         sortBy={sortBy}
       />
-      <FilterButton buttonName="Diet" />
-      <FilterButton buttonName="Meal" />
+
+      <Button
+        className={dietBtnStyle}
+        onClick={() => toggleFilterButton("Diet")}
+      >
+        {diet === "None" ? "Diet" : diet}
+        <Arrow className={dietArrowStyle} />
+      </Button>
+
+      <Button
+        className={mealBtnStyle}
+        onClick={() => toggleFilterButton("Meal")}
+      >
+        {meal === "All Meals" ? "Meal" : meal}
+        <Arrow className={mealArrowStyle} />
+      </Button>
     </div>
   );
 };
 
 const mapSateToProps = state => {
   return {
-    buttonToggled: state.browseRecipes.toggleFilterButton,
-    sortBy: state.browseRecipes.browseData.sort
+    activeFilterBtn: state.browseRecipes.toggleFilterButton,
+    sortBy: state.browseRecipes.browseData.sort,
+    diet: state.browseRecipes.browseData.diet,
+    meal: state.browseRecipes.browseData.meal
   };
 };
 
