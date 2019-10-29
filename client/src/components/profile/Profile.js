f07642f18e48a39e20bd840a720ffb33bc911bd4
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-10-28 22:02:42 -0400

    Create a profile header component and clean up profile component

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -11,16 +11,12 @@ import {
 import { getUser } from "../../actions/authActions";
 
 // Components
+import Header from "./Header";
 import RecipeContainer from "./RecipeContainer";
 import SortBy from "./SortBy";
 import { SortButton } from "../sort-button/SortButton";
 import EditProfile from "./edit-profile/EditProfile";
 import CreateRecipe from "./createRecipe/CreateRecipe";
-import { Button } from "../Button";
-import { ReactComponent as UserIcon } from "../../images/userProfile.svg";
-import { ReactComponent as WhiskIcon } from "../../images/WhiskIcon.svg";
-import { ReactComponent as SavedIcon } from "../../images/savedRecipes.svg";
-import { ReactComponent as MyRecipesIcon } from "../../images/myRecipes.svg";
 
 // Styles
 import "./profile-styles.css";
@@ -36,9 +32,8 @@ class Profile extends React.Component {
 
   componentDidMount() {
     this.props.getUser();
-    const user_id = this.props.auth.isAuthenticated
-      ? this.props.auth.user.user_id
-      : null;
+    const user_id =
+      this.props.auth.isAuthenticated && this.props.auth.user.user_id;
     this.props.getMyRecipes(user_id);
     this.props.getSavedRecipes(user_id);
   }
@@ -49,10 +44,14 @@ class Profile extends React.Component {
 
   render() {
     const page = this.state.page;
-    const { sortActive, sortBy } = this.props.savedRecipes;
+    const username = this.props.username.toLowerCase();
+    const {
+      sortActive,
+      sortBy,
+      recipes: savedRecipes
+    } = this.props.savedRecipes;
+    const { recipes: myRecipes } = this.props.myRecipes;
 
-    const savedActive = page === "saved" ? "active" : "";
-    const myRecipesActive = page === "myRecipes" ? "active" : "";
     return (
       <Switch>
         <Route path="/profile/edit" component={EditProfile} />
@@ -61,75 +60,15 @@ class Profile extends React.Component {
           component={() => {
             return (
               <div className="profile">
-                <div
-                  style={{
-                    display: "grid",
-                    placeItems: "cetner",
-                    backgroundColor: "#FFF",
-                    position: "fixed",
-                    width: "100%",
-                    height: "2.1rem"
-                  }}
-                >
-                  <WhiskIcon
-                    style={{
-                      width: "5rem",
-                      margin: ".2rem auto"
-                    }}
-                  />
-                </div>
-                <div className="profile-header">
-                  <Link
-                    style={{
-                      display: "grid",
-                      placeItems: "center"
-                    }}
-                    to="/profile/edit"
-                    className="profile-edit"
-                  >
-                    <UserIcon style={{ width: "3.5rem" }} />
-                    <div
-                      style={{
-                        color: "#0172C4",
-                        fontSize: "1rem",
-                        margin: ".3rem"
-                      }}
-                    >
-                      Edit
-                    </div>
-                  </Link>
-                  <h2
-                    style={{
-                      fontSize: "1.5rem",
-                      fontWeight: "900",
-                      margin: 0,
-                      marginBottom: "1rem",
-                      color: "#313131"
-                    }}
-                  >
-                    {this.props.username.toLowerCase()}
-                  </h2>
-                </div>
-
-                <div className="s-mr-toggle">
-                  <Button
-                    className={`authButton ${savedActive}`}
-                    onClick={() => this.handleClick("saved")}
-                  >
-                    <SavedIcon style={{ width: "1.5rem" }} />
-                  </Button>
-
-                  <Button
-                    className={`authButton ${myRecipesActive}`}
-                    onClick={() => this.handleClick("myRecipes")}
-                  >
-                    <MyRecipesIcon style={{ width: "1.5rem" }} />
-                  </Button>
-                </div>
+                <Header
+                  page={page}
+                  onClick={this.handleClick}
+                  username={username}
+                />
                 <div>
                   {page === "saved" ? (
                     <RecipeContainer
-                      recipes={this.props.savedRecipes.recipes}
+                      recipes={savedRecipes}
                       containerName="Saved Recipes"
                     >
                       {sortActive && <SortBy />}
@@ -147,23 +86,11 @@ class Profile extends React.Component {
                     </RecipeContainer>
                   ) : (
                     <RecipeContainer
-                      recipes={this.props.myRecipes.recipes}
+                      recipes={myRecipes}
                       containerName="My Recipes"
                     >
                       <Link to="/profile/create-recipe">
-                        <div
-                          style={{
-                            backgroundColor: "#0172C4",
-                            color: "#FFF",
-                            padding: ".7rem 5rem",
-                            margin: "1.5rem",
-                            fontWeight: "bold",
-                            borderRadius: "10rem",
-                            textAlign: "center"
-                          }}
-                        >
-                          Create Recipe
-                        </div>
+                        <div className="create-recipe-btn">Create Recipe</div>
                       </Link>
                     </RecipeContainer>
                   )}
