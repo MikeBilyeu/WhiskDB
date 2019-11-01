812a17003aef196178359db1373f446a2117a91d
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-11-01 16:04:09 -0400

    Categories compoent working

--- client/src/components/profile/createRecipe/Categories.js
+++ client/src/components/profile/createRecipe/Categories.js
@@ -1,21 +1,83 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Categories = () => {
-  /*
-  pass in categories as prop from parent
+// Categories
+import { Button } from "../../Button";
+import { ReactComponent as Remove } from "../../../images/removeDark.svg";
 
-  keep track of categories on local state in arr
-  map over local catArr render ul
-  li button onClick this.props.fields.push() and setState local
+class Categories extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [
+        "vegetarian",
+        "vegan",
+        "breakfast",
+        "lunch",
+        "dinner",
+        "appetizer",
+        "dessert",
+        "beverage"
+      ]
+    };
+  }
 
-  render ul
-  this.props.fields.map
-  li button onClick this.props.fields.remove(index) and setState local
+  handleAddClick = category => {
+    // Add to redux state
+    this.props.change("categories", [...this.props.categories, category]);
 
+    this.setState(({ categories }) => {
+      // Remove from local state
+      return { categories: categories.filter(el => el !== category) };
+    });
+  };
 
+  handleRemoveClick = category => {
+    // Remove from redux state
+    this.props.change(
+      "categories",
+      this.props.categories.filter(el => el !== category)
+    );
 
-  */
-  return <div></div>;
-};
+    // Add to local state
+    this.setState(({ categories }) => {
+      return { categories: [...categories, category] };
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Categories</h2>
+        <label>Categories</label>
+        <ul>
+          {this.state.categories.map((category, index, arr) => {
+            return (
+              <li key={"add " + index}>
+                <Button onClick={() => this.handleAddClick(category)}>
+                  {category}
+                </Button>
+              </li>
+            );
+          })}
+        </ul>
+
+        <label>Remove Categories</label>
+        <ul>
+          {this.props.categories &&
+            this.props.categories.map((category, index, arr) => {
+              return (
+                <li key={"remove " + index}>
+                  <Button onClick={() => this.handleRemoveClick(category)}>
+                    {category}
+                    <Remove style={{ width: ".8rem" }} />
+                  </Button>
+                </li>
+              );
+            })}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default Categories;
