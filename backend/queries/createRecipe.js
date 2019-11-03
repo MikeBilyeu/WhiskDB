03669c0798b3930507a6eff1ecfa603a71fdb05c
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-11-03 14:23:35 -0500

    Fix category instert query

--- backend/queries/createRecipe.js
+++ backend/queries/createRecipe.js
@@ -145,62 +145,42 @@ const createRecipe = (request, response) => {
 
   pool.connect().then(client => {
     let recipe_id = null;
-    return (
-      client
-        .query(
-          `INSERT INTO recipes
+    return client
+      .query(
+        `INSERT INTO recipes
         (created_by, title, servings, total_time_mins, footnote,
         private, directions, ingredients, keywords, document_vectors)
         VALUES ($1, CAST($2 AS VARCHAR), $3, $4, $5, $6, $7, $8,
         CAST($9 AS VARCHAR[]), (to_tsvector($2) ||
         to_tsvector(array_to_string($9, ' ')))) RETURNING recipe_id`,
-          [
-            created_by,
-            title,
-            servings,
-            total_time_mins,
-            footnote,
-            privateRecipe,
-            directions,
-            metricIngredients,
-            keywords
-          ]
-        )
-        // .then(res => {
-        //   recipe_id = res.rows[0].recipe_id;
-        //   // loop through the keys of the category object
-        //   for (let i = 0, keys = Object.keys(categories); i < keys.length; i++) {
-        //     // loop through sub categories
-        //     for (
-        //       let j = 0, subKeys = Object.keys(categories[keys[i]]);
-        //       j < subKeys.length;
-        //       j++
-        //     ) {
-        //       if (categories[keys[i]][subKeys[j]] === true) {
-        //         client
-        //           .query(
-        //             "SELECT category_id FROM categories WHERE category_name = $1",
-        //             [subKeys[j]]
-        //           )
-        //           .then(res => {
-        //             client.query(
-        //               "INSERT INTO recipes_join_categories (recipe, category) VALUES ($1, $2)",
-        //               [recipe_id, res.rows[0].category_id]
-        //             );
-        //           });
-        //       }
-        //     }
-        //   }
-        // })
-        .then(res => {
-          recipe_id = res.rows[0].recipe_id;
-          return response.status(200).send({ recipe_id: recipe_id });
-        })
-        .catch(e => {
-          client.release();
-          console.error(e);
-        })
-    );
+        [
+          created_by,
+          title,
+          servings,
+          total_time_mins,
+          footnote,
+          privateRecipe,
+          directions,
+          metricIngredients,
+          keywords
+        ]
+      )
+      .then(res => {
+        recipe_id = res.rows[0].recipe_id;
+        categories.forEach(category => {
+          client.query(
+            `INSERT INTO recipes_join_categories (recipe, category)
+               VALUES ($1, (SELECT category_id FROM categories
+               WHERE category_name = $2))`,
+            [recipe_id, category]
+          );
+        });
+        return response.status(200).send({ recipe_id: recipe_id });
+      })
+      .catch(e => {
+        client.release();
+        console.error(e);
+      });
   });
 };
 
