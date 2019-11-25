3d47962cbf38d1be297524ed9f8d46e7d0e4f5ca
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-11-25 15:38:03 -0500

    fix styles

--- frontend/src/components/recipe/review/details/star-votes/details-star-votes-view.js
+++ frontend/src/components/recipe/review/details/star-votes/details-star-votes-view.js
@@ -3,37 +3,17 @@ import React from "react";
 const StarVotes = ({ star, num_reviews, votes }) => {
   num_reviews = num_reviews || 1;
   return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: "3rem 1fr 3rem",
-        placeItems: "center",
-        margin: ".5rem 0"
-      }}
-    >
+    <div className="vote-bar">
       <span>{star} star</span>
-      <div
-        style={{
-          overflow: "hidden",
-          height: "1.3rem",
-          width: "100%",
-          border: "solid #E3E3E3 .1rem",
-          backgroundColor: "#F5F5F5",
-          display: "grid",
-          AlignItems: "center",
-          borderRadius: ".2rem"
-        }}
-      >
+      <div className="vote-percent-bar">
         <div
+          className="votes-percent"
           style={{
-            backgroundColor: "#FFB652",
-            height: "100%",
-            minHeight: "100%",
             width: `${(votes / num_reviews) * 100}%`
           }}
         ></div>
       </div>
-      <span style={{ justifySelf: "end" }}>{`${Math.round(
+      <span className="percent-num">{`${Math.round(
         (votes / num_reviews) * 100
       )}%`}</span>
     </div>
