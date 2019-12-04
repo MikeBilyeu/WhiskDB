1432a50f8283504440b26c010b0c722adf615d02
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-12-04 14:01:33 -0500

    Refactor getEmails

--- backend/queries/getEmails.js
+++ backend/queries/getEmails.js
@@ -1,35 +1,21 @@
-const Pool = require("pg").Pool;
+const pool = require("../utils/connectPool");
 
-const keys = require("../config/keys");
-
-// Connect to pool
-const pool = new Pool({
-  user: keys.user,
-  host: keys.host,
-  database: keys.database,
-  password: keys.password,
-  port: keys.port
-});
-
-const getEmails = (request, response) => {
+const getEmails = async (request, response) => {
   const { email } = request.query;
 
-  pool.connect().then(client => {
-    return client
-      .query("SELECT * FROM users WHERE LOWER(email) = LOWER($1)", [email])
-      .then(res => {
-        client.release();
-        if (res.rowCount > 0) {
-          return response.status(409).json({ usernmae: "Email is in use" });
-        } else {
-          return response.status(200).json({ usernmae: "Email is not in use" });
-        }
-      })
-      .catch(e => {
-        client.release();
-        return response.status(500);
-      });
-  });
+  try {
+    const client = await pool.connect();
+    await client.query(`SELECT * FROM users WHERE LOWER(email) = LOWER($1)`, [
+      email
+    ]);
+    client.release();
+    if (res.rowCount > 0) {
+      return response.status(409).json({ usernmae: "Email is in use" });
+    }
+    return response.status(200).json({ usernmae: "Email is not in use" });
+  } catch (err) {
+    return response.status(500).json(err);
+  }
 };
 
 module.exports = {
