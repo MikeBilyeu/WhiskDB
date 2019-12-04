7a58fa1987476a10be048ade8dae3239542019d0
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-12-04 13:47:50 -0500

    Refector editRecipe

--- backend/queries/editRecipe.js
+++ backend/queries/editRecipe.js
@@ -1,16 +1,7 @@
-const Pool = require("pg").Pool;
-const keys = require("../config/keys");
-// Connect to pool
-const pool = new Pool({
-  user: keys.user,
-  host: keys.host,
-  database: keys.database,
-  password: keys.password,
-  port: keys.port
-});
+const pool = require("../utils/connectPool");
 
-const editRecipe = (request, response) => {
-  const { user_id } = request.user;
+const editRecipe = async (request, response) => {
+  const { user_id } = request.user; // Get user_id from auth
   const {
     recipe_id,
     created_by,
@@ -31,33 +22,30 @@ const editRecipe = (request, response) => {
   if (user_id !== created_by) {
     return response.status(401).send("You can't edit this recipe");
   }
-  pool.connect().then(client => {
-    return client
-      .query(
-        `UPDATE recipes
-         SET title = $1, servings = $2, ingredients = $3, directions = $4,
-          footnote = $5, categories = $6, keywords = $7, total_time_mins = $8
-         WHERE recipe_id = $9`,
-        [
-          title,
-          servings,
-          ingredients,
-          directions,
-          footnote,
-          categories,
-          keywords,
-          total_time_mins,
-          recipe_id
-        ]
-      )
-      .then(() => {
-        return response.status(200).send("Changes saved!");
-      })
-      .catch(e => {
-        client.release();
-        console.log(e);
-      });
-  });
+
+  try {
+    const client = pool.connect();
+    await client.query(
+      `UPDATE recipes
+     SET title = $1, servings = $2, ingredients = $3, directions = $4,
+      footnote = $5, categories = $6, keywords = $7, total_time_mins = $8
+     WHERE recipe_id = $9`,
+      [
+        title,
+        servings,
+        ingredients,
+        directions,
+        footnote,
+        categories,
+        keywords,
+        total_time_mins,
+        recipe_id
+      ]
+    );
+    return response.status(200).send("Changes saved!");
+  } catch (err) {
+    return response.status(500).json(err);
+  }
 };
 
 module.exports = {
