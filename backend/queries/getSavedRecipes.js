e5a240e6e589a02e0e45b7ce933f6b7f7f30aa0f
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-12-04 14:31:21 -0500

    Refactor getSavedRecipes

--- backend/queries/getSavedRecipes.js
+++ backend/queries/getSavedRecipes.js
@@ -1,39 +1,31 @@
-const Pool = require("pg").Pool;
+const pool = require("../utils/connectPool");
 
-const keys = require("../config/keys");
-
-const pool = new Pool({
-  user: keys.user,
-  host: keys.host,
-  database: keys.database,
-  password: keys.password,
-  port: keys.port
-});
-
-const getSavedRecipes = (request, response) => {
+const getSavedRecipes = async (request, response) => {
   const { user_id } = request.user;
-  //connect pool
-  pool.connect().then(client => {
-    return client
-      .query(
-        `SELECT r.recipe_id, r.title, r.image_url, r.total_time_mins, COALESCE(AVG(rw.rating), 0) AS rating,
-        CAST(count(rw.*) AS INTEGER) AS num_reviews
-        FROM recipes r
-        LEFT JOIN reviews rw ON r.recipe_id = rw.recipe_id
-        WHERE
-        r.recipe_id IN ( SELECT recipe_saved FROM saved_recipes WHERE
-        saved_by = $1 ) GROUP BY r.recipe_id, rw.recipe_id;`,
-        [user_id]
-      )
-      .then(res => {
-        client.release();
-        response.status(200).json(res.rows);
-      })
-      .catch(err => {
-        client.release();
-        console.log(err.stack);
-      });
-  });
+  try {
+    const client = await pool.connect();
+    const res = await client.query(
+      `SELECT r.recipe_id,
+             r.title,
+             r.image_url,
+             r.total_time_mins,
+             COALESCE(AVG(rw.rating), 0) AS rating,
+             CAST(count(rw.*) AS INTEGER) AS num_reviews
+      FROM recipes r
+      LEFT JOIN reviews rw ON r.recipe_id = rw.recipe_id
+      WHERE r.recipe_id IN
+          (SELECT recipe_saved
+           FROM saved_recipes
+           WHERE saved_by = $1 )
+      GROUP BY r.recipe_id,
+               rw.recipe_id;`,
+      [user_id]
+    );
+    client.release();
+    response.status(200).json(res.rows);
+  } catch (err) {
+    response.status(500).json(errs);
+  }
 };
 
 module.exports = {
