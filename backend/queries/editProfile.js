06b16f5da66395dfd8b9330888143dd30b695159
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-12-04 13:41:45 -0500

    Refacotr editProfile

--- backend/queries/editProfile.js
+++ backend/queries/editProfile.js
@@ -1,19 +1,8 @@
-const Pool = require("pg").Pool;
+const pool = require("../utils/connectPool");
 const jwt = require("jsonwebtoken");
-
-const keys = require("../config/keys");
-// Connect to pool
-const pool = new Pool({
-  user: keys.user,
-  host: keys.host,
-  database: keys.database,
-  password: keys.password,
-  port: keys.port
-});
-
 const validateUsername = require("../validation/usernameValidate");
 
-const editProfile = (request, response) => {
+const editProfile = async (request, response) => {
   const { full_name, username } = request.body;
   const { user_id } = request.user; // get user_id from auth
   const errors = validateUsername(username);
@@ -22,40 +11,38 @@ const editProfile = (request, response) => {
     return response.status(400).json(errors);
   }
 
-  pool.connect().then(client => {
-    return client
-      .query(
-        "UPDATE users SET full_name = $1, username = $2 WHERE user_id = $3",
-        [full_name, username, user_id]
-      )
-      .then(() => {
-        const payload = {
-          user_id: user_id,
-          username: username,
-          full_name: full_name
-        };
+  try {
+    const client = await pool.connect;
+    await client.query(
+      `UPDATE users SET full_name = $1,
+      username = $2 WHERE user_id = $3`,
+      [full_name, username, user_id]
+    );
 
-        // Sign token
-        jwt.sign(
-          payload,
-          keys.secretOrKey,
-          {
-            expiresIn: 31556926 // 1 year in seconds
-          },
-          (err, token) => {
-            if (err) throw err;
-            response.json({
-              success: true,
-              token: "Bearer " + token
-            });
-          }
-        );
-      })
-      .catch(e => {
-        client.release();
-        console.log(e);
-      });
-  });
+    const payload = {
+      user_id: user_id,
+      username: username,
+      full_name: full_name
+    };
+
+    // Sign token
+    jwt.sign(
+      payload,
+      keys.secretOrKey,
+      {
+        expiresIn: 31556926 // 1 year in seconds
+      },
+      (err, token) => {
+        if (err) throw err;
+        response.json({
+          success: true,
+          token: "Bearer " + token
+        });
+      }
+    );
+  } catch (err) {
+    return response.status(400).json(err);
+  }
 };
 
 module.exports = {
