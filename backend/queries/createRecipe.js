790b2aebafda11aca0c70f95bfac16f0267096a6
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-12-04 13:33:42 -0500

    Refactor createRecipe

--- backend/queries/createRecipe.js
+++ backend/queries/createRecipe.js
@@ -1,18 +1,9 @@
-const Pool = require("pg").Pool;
-const keys = require("../config/keys");
-// Connect to pool
-const pool = new Pool({
-  user: keys.user,
-  host: keys.host,
-  database: keys.database,
-  password: keys.password,
-  port: keys.port
-});
+const pool = require("../utils/connectPool");
 
 // Load input validation
-const validateRecipeInput = require("../validation/recipe");
+// const validateRecipeInput = require("../utils/validation/recipe");
 
-const createRecipe = (request, response) => {
+const createRecipe = async (request, response) => {
   const {
     time,
     servings,
@@ -37,52 +28,43 @@ const createRecipe = (request, response) => {
   const timeMinutes = time.minutes > 0 ? parseInt(time.minutes) : 0;
   const total_time_mins = timeHours * 60 + timeMinutes;
 
-  pool.connect().then(client => {
-    let recipe_id = null;
-    return client
-      .query(
-        `INSERT INTO recipes
-        (
-          created_by, title, servings, total_time_mins,
-          footnote, directions, ingredients, keywords,
-          categories, image_url, document_vectors
-        )
-        VALUES (
-          $1, CAST($2 AS VARCHAR), $3, $4, $5, $6, $7,
-          CAST($8 AS VARCHAR[]), $9, $10, (to_tsvector($2) ||
-          to_tsvector(array_to_string($8, ' ')))
-        )
-        RETURNING recipe_id`,
-        [
-          created_by,
-          title,
-          servings,
-          total_time_mins,
-          footnote,
-          directions,
-          ingredients,
-          keywords,
-          categories,
-          image_url
-        ]
-      )
-      .then(res => {
-        recipe_id = res.rows[0].recipe_id;
-        categories.forEach(category => {
-          client.query(
-            `INSERT INTO recipes_join_categories (recipe, category)
-            VALUES ($1, (SELECT category_id FROM categories
-              WHERE category_name = $2))`,
-            [recipe_id, category]
-          );
-        });
-        return response.status(200).send({ recipe_id: recipe_id });
-      })
-      .catch(e => {
-        client.release();
-        console.error(e);
-      });
-  });
+  try {
+    const client = await pool.connect();
+    const res = await client.query(
+      `INSERT INTO recipes( created_by, title, servings, total_time_mins,
+        footnote, directions, ingredients, keywords, categories, image_url,
+        document_vectors ) VALUES ( $1, CAST($2 AS VARCHAR), $3, $4, $5, $6,
+        $7, CAST($8 AS VARCHAR[]), $9, $10, ( to_tsvector($2) || to_tsvector( 
+        array_to_string($8, ' ') ) ) ) RETURNING recipe_id`,
+      [
+        created_by,
+        title,
+        servings,
+        total_time_mins,
+        footnote,
+        directions,
+        ingredients,
+        keywords,
+        categories,
+        image_url
+      ]
+    );
+
+    const recipe_id = res.rows[0].recipe_id;
+
+    categories.forEach(category => {
+      client.query(
+        `INSERT INTO recipes_join_categories (recipe, category)
+         VALUES ($1, (SELECT category_id FROM categories
+         WHERE category_name = $2))`,
+        [recipe_id, category]
+      );
+    });
+    client.release();
+    return response.status(200).send({ recipe_id: recipe_id });
+  } catch (err) {
+    return response.status(400).json(err);
+  }
 };
 
 module.exports = {
