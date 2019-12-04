58c8d286395603c2bf8ed121e62e09ae66b3fcfd
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-12-03 20:27:14 -0500

    Remove console.log

--- frontend/src/components/profile/create-recipe/header/create-recipe-header-view.js
+++ frontend/src/components/profile/create-recipe/header/create-recipe-header-view.js
@@ -3,7 +3,6 @@ import { ReactComponent as Arrow } from "../../../../assets/images/arrowLeft.svg
 import "./create-recipe-header.scss";
 
 const Header = props => {
-  console.log(props);
   return (
     <div className="header">
       <Arrow className="back-btn" onClick={props.onClick} />
