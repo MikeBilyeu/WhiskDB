8acc73a466e16b5dbd3395175fe9390c73b18566
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-12-04 13:35:26 -0500

    Refactor deleteUser

--- backend/queries/deleteUser.js
+++ backend/queries/deleteUser.js
@@ -1,19 +1,8 @@
-const Pool = require("pg").Pool;
+const pool = require("../utils/connectPool");
 const jwt = require("jsonwebtoken");
-
-const keys = require("../config/keys");
-// Connect to pool
-const pool = new Pool({
-  user: keys.user,
-  host: keys.host,
-  database: keys.database,
-  password: keys.password,
-  port: keys.port
-});
 
 const deleteUser = (request, response) => {
   const { user_id } = request.user; // get user_id from auth
-
   pool.connect().then(client => {
     return client
       .query(`DELETE FROM users WHERE user_id = $1`, [user_id])
