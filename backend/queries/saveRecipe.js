7a9a4372a68e9542581e995530ccb5c99b329520
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-12-04 15:08:33 -0500

    Refactor saveRecipe

--- backend/queries/saveRecipe.js
+++ backend/queries/saveRecipe.js
@@ -1,49 +1,39 @@
-const Pool = require("pg").Pool;
+const pool = require("../utils/connectPool");
 
-const keys = require("../config/keys");
-
-const pool = new Pool({
-  user: keys.user,
-  host: keys.host,
-  database: keys.database,
-  password: keys.password,
-  port: keys.port
-});
-
-const saveRecipe = (request, response) => {
+const saveRecipe = async (request, response) => {
   const { recipe_id } = request.body;
-  const { user_id } = request.user;
+  const { user_id } = request.user; // Get user_id from auth
+  try {
+    const client = await pool.connect();
+    const res = await client.query(
+      `SELECT *
+        FROM saved_recipes
+        WHERE saved_by = $1
+          AND recipe_saved = $2`,
+      [user_id, recipe_id]
+    );
 
-  pool.connect().then(client => {
-    return client
-      .query(
-        "SELECT * FROM saved_recipes WHERE saved_by = $1 AND recipe_saved = $2",
+    if (res.rowCount > 0) {
+      // Delete saved recipe from db
+      await client.query(
+        `DELETE
+          FROM saved_recipes
+          WHERE saved_by = $1
+            AND recipe_saved = $2`,
         [user_id, recipe_id]
-      )
-      .then(res => {
-        if (res.rowCount > 0) {
-          //delete from db
-          client
-            .query(
-              "DELETE FROM saved_recipes WHERE saved_by = $1 AND recipe_saved = $2",
-              [user_id, recipe_id]
-            )
-            .then(res => response.status(200).send("unsaved"));
-        } else if (res.rowCount === 0) {
-          // add to db
-          client
-            .query(
-              "INSERT INTO saved_recipes (saved_by, recipe_saved) VALUES ($1, $2)",
-              [user_id, recipe_id]
-            )
-            .then(res => response.status(200).send("saved"));
-        }
-      })
-      .catch(err => {
-        client.release();
-        console.log(err);
-      });
-  });
+      );
+      response.status(200).send("unsaved");
+    }
+    // Add saved recipe to db
+    await client.query(
+      `INSERT INTO saved_recipes (saved_by, recipe_saved)
+            VALUES ($1, $2)`,
+      [user_id, recipe_id]
+    );
+    response.status(200).send("saved");
+  } catch (err) {
+    response.status(500).json(err);
+  }
 };
 
 module.exports = {
