f6012f911964da6eed630cca788f2add71552caa
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-12-04 14:28:07 -0500

    Refactor getRecipe

--- backend/queries/getRecipe.js
+++ backend/queries/getRecipe.js
@@ -1,45 +1,33 @@
-const Pool = require("pg").Pool;
+const pool = require("../utils/connectPool");
 
-const keys = require("../config/keys");
-
-// Connect to pool
-const pool = new Pool({
-  user: keys.user,
-  host: keys.host,
-  database: keys.database,
-  password: keys.password,
-  port: keys.port
-});
-
-const getRecipe = (request, response) => {
+const getRecipe = async (request, response) => {
   const { recipe_id, user_id } = request.query;
-
-  pool.connect().then(client => {
-    return client
-      .query(
-        `SELECT r.*,
-         TO_CHAR(r.created_at, 'Mon fmDD, YYYY') AS date_created,
-         u.username AS username, sr.saved_by = $2 saved,
-         COALESCE(AVG(rw.rating), 0) AS rating,
-         CAST(count(rw.*) AS INTEGER) AS num_reviews
-         FROM recipes r
-          LEFT JOIN users u ON u.user_id = r.created_by
-          LEFT JOIN saved_recipes sr ON r.recipe_id = sr.recipe_saved AND sr.saved_by = $2
-          LEFT JOIN reviews rw ON r.recipe_id = rw.recipe_id
-          WHERE r.recipe_id = $1
-          GROUP BY r.recipe_id, u.user_id, sr.saved_by;`,
-        [recipe_id, user_id]
-      )
-      .then(res => {
-        client.release();
-        if (res.rows[0]) {
-          response.status(200).json(res.rows[0]);
-        }
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
+  try {
+    const client = await pool.connect();
+    const res = await client.query(
+      `SELECT r.*,
+             TO_CHAR(r.created_at, 'Mon fmDD, YYYY') AS date_created,
+             u.username AS username,
+             sr.saved_by = $2 saved,
+             COALESCE(AVG(rw.rating), 0) AS rating,
+             CAST(count(rw.*) AS INTEGER) AS num_reviews
+      FROM recipes r
+      LEFT JOIN users u ON u.user_id = r.created_by
+      LEFT JOIN saved_recipes sr ON r.recipe_id = sr.recipe_saved
+      AND sr.saved_by = $2
+      LEFT JOIN reviews rw ON r.recipe_id = rw.recipe_id
+      WHERE r.recipe_id = $1
+      GROUP BY r.recipe_id,
+               u.user_id,
+               sr.saved_by;`,
+      [recipe_id, user_id]
+    );
+    if (res.rows[0]) {
+      return response.status(200).json(res.rows[0]);
+    }
+  } catch (err) {
+    return response.status(500).json(err);
+  }
 };
 
 module.exports = {
