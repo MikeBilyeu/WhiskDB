2ca8fdcca66b310b4b03d63852119668c701011f
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-12-04 14:48:19 -0500

    Refactor deleteUser

--- backend/queries/deleteUser.js
+++ backend/queries/deleteUser.js
@@ -1,19 +1,20 @@
 const pool = require("../utils/connectPool");
 const jwt = require("jsonwebtoken");
 
-const deleteUser = (request, response) => {
-  const { user_id } = request.user; // get user_id from auth
-  pool.connect().then(client => {
-    return client
-      .query(`DELETE FROM users WHERE user_id = $1`, [user_id])
-      .then(res => {
-        return response.status(200).json("User removed");
-      })
-      .catch(e => {
-        client.release();
-        console.log(e);
-      });
-  });
+const deleteUser = async (request, response) => {
+  const { user_id } = request.user; // Get user_id from auth
+  try {
+    const client = await pool.connect();
+    await client.query(
+      `DELETE
+    FROM users
+    WHERE user_id = $1`,
+      [user_id]
+    );
+    response.status(200).json("User removed");
+  } catch (err) {
+    response.status(500).json(err);
+  }
 };
 
 module.exports = {
