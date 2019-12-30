5fd25eedc159ffca2b5f309b91d97d355207a5e1
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-12-30 13:58:17 -0500

    Add plus icon to profile header

--- frontend/src/components/profile/home/header/home-header-view.jsx
+++ frontend/src/components/profile/home/header/home-header-view.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import { ReactComponent as WhiskIcon } from "../../../../assets/images/WhiskIcon.svg";
-import SortButton from "../../../sort-button";
+import { ReactComponent as AddIcon } from "../../../../assets/images/add.svg";
 import userPhoto from "../../../../assets/images/user-photo.jpg";
 import "./home-header.scss";
 
@@ -17,6 +16,7 @@ class Header extends React.PureComponent {
           <h2>{this.props.fullName || null}</h2>
           <Link className="create-recipe-link" to="/profile/create-recipe">
             <div className="create-recipe-btn">Create Recipe</div>
+            <AddIcon style={{ width: "1.5rem" }} />
           </Link>
         </div>
       </div>
