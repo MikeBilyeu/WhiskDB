400ebb21d9b56d616992579b618f0224b98bb7b0
Mike Bilyeu <mikebilyeu512@gmail.com>
2019-12-30 15:25:15 -0500

    Refactor

--- frontend/src/selectors/ingredient-selector/utils/formatIngredientList.js
+++ frontend/src/selectors/ingredient-selector/utils/formatIngredientList.js
@@ -18,73 +18,67 @@ export const splitIngredientStr = ingredientStr => {
   return { amount, unit, ingredient };
 };
 
+const getMetricAmount = (amount, unit) => {
+  // using eval to get the decimal of mixed fractions
+  let decimalAmount = eval(amount.split(/[ -]/).join("+"));
+  let metricUnit = "";
+  switch (true) {
+    case regEx.cup.test(unit):
+      decimalAmount *= 236.588;
+      metricUnit = "ml.";
+      break;
+    case regEx.tablespoon.test(unit):
+    case regEx.T.test(unit):
+      decimalAmount *= 14.787;
+      metricUnit = "ml.";
+      break;
+    case regEx.teaspoon.test(unit):
+    case regEx.t.test(unit):
+      decimalAmount *= 4.929;
+      metricUnit = "ml.";
+      break;
+    case regEx.milliliter.test(unit):
+      decimalAmount *= 1;
+      metricUnit = "ml.";
+      break;
+    case regEx.liter.test(unit):
+      decimalAmount *= 1000;
+      metricUnit = "ml.";
+      break;
+    case regEx.ounce.test(unit):
+      decimalAmount *= 28.35;
+      metricUnit = "g.";
+      break;
+    case regEx.pound.test(unit):
+      decimalAmount *= 453.592;
+      metricUnit = "g.";
+      break;
+    case regEx.kilogram.test(unit):
+      decimalAmount *= 1000;
+      metricUnit = "g.";
+      break;
+    case regEx.gram.test(unit):
+      decimalAmount *= 1;
+      metricUnit = "g.";
+      break;
+    default:
+      break;
+  }
+  return { decimalAmount, metricUnit };
+};
+
 //convert ingredient amount from us to metric
 const formatIngredientList = ingredientList => {
-  let formatedList = ingredientList.map(ingredientStr => {
-    let { amount, unit, ingredient } = splitIngredientStr(ingredientStr);
-    // using eval to get the decimal of mixed fractions
-    let decimalAmount = eval(amount.split(/[ -]/).join("+"));
-
-    //console.log("decimalAmount: ", decimalAmount);
-    // may have to round decimal to nearest 1000th
-
-    let metricUnit = unit;
-
-    if (unit) {
-      switch (true) {
-        case regEx.cup.test(unit):
-          decimalAmount *= 236.588;
-          metricUnit = "ml.";
-          break;
-        case regEx.tablespoon.test(unit):
-        case regEx.T.test(unit):
-          decimalAmount *= 14.787;
-          metricUnit = "ml.";
-          break;
-        case regEx.teaspoon.test(unit):
-        case regEx.t.test(unit):
-          decimalAmount *= 4.929;
-          metricUnit = "ml.";
-          break;
-        case regEx.milliliter.test(unit):
-          decimalAmount *= 1;
-          metricUnit = "ml.";
-          break;
-        case regEx.liter.test(unit):
-          decimalAmount *= 1000;
-          metricUnit = "ml.";
-          break;
-        case regEx.ounce.test(unit):
-          decimalAmount *= 28.35;
-          metricUnit = "g.";
-          break;
-        case regEx.pound.test(unit):
-          decimalAmount *= 453.592;
-          metricUnit = "g.";
-          break;
-        case regEx.kilogram.test(unit):
-          decimalAmount *= 1000;
-          metricUnit = "g.";
-          break;
-        case regEx.gram.test(unit):
-          decimalAmount *= 1;
-          metricUnit = "g.";
-          break;
-        default:
-          break;
-      }
-      unit = metricUnit;
-    }
-
-    amount = decimalAmount.toString();
+  return ingredientList.map(ingredientStr => {
+    const { amount, unit, ingredient } = splitIngredientStr(ingredientStr);
+    const { decimalAmount, metricUnit } = getMetricAmount(amount, unit);
 
     return {
-      amount: amount,
-      unit: unit,
+      amount: decimalAmount.toString(),
+      unit: metricUnit,
       ingredient: ingredient
     };
   });
-  return formatedList;
 };
 
 export default formatIngredientList;
