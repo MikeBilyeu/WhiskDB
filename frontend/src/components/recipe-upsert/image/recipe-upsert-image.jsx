d074b73754d7d4039e50f5b861235c5d9e5d0666
Mike Bilyeu <mikebilyeu512@gmail.com>
2020-01-15 19:29:41 -0500

    Image displaying but not in app state

--- frontend/src/components/recipe-upsert/image/recipe-upsert-image.jsx
+++ frontend/src/components/recipe-upsert/image/recipe-upsert-image.jsx
@@ -1,13 +1,47 @@
 import React from "react";
-import Button from "../../button";
-import styles from "../recipe-upsert.module.scss";
+import { getFormValues } from "redux-form";
+import { connect } from "react-redux";
 
-class ImageUpload extends React.Component {
-  render() {
-    return (
-      <Button className={styles.imageInputBtn}>Choose Photo to Upload</Button>
-    );
-  }
-}
+const ImageUpload = ({ input: { onChange }, ...props }) => {
+  const image =
+    props.formValues && props.formValues.image ? props.formValues.image : null;
+  return (
+    <div>
+      <label className="imageInput" htmlFor="ImageUpload">
+        <i className="upload icon" />
+        Upload Image
+      </label>
+      <input
+        type="file"
+        accept="image/.jpg, image/.png, image/.jpeg"
+        onChange={e => {
+          console.log(e.target.files[0]);
+          onChange(e.target.files[0]);
+        }}
+        id="ImageUpload"
+        style={{ display: "none" }}
+      />
+      {image ? (
+        <img
+          alt=""
+          src={URL.createObjectURL(image)}
+          style={{
+            objectFit: "cover",
+            objectPosition: "center",
+            width: "20rem",
+            height: "12rem",
+            borderRadius: ".1rem",
+            display: "block",
+            margin: "1rem auto"
+          }}
+        />
+      ) : null}
+    </div>
+  );
+};
 
-export default ImageUpload;
+const mapSateToProps = state => {
+  return { formValues: getFormValues("newRecipe")(state) };
+};
+
+export default connect(mapSateToProps)(ImageUpload);
--- frontend/src/components/recipe-upsert/recipe-upsert-container.jsx
+++ frontend/src/components/recipe-upsert/recipe-upsert-container.jsx
@@ -9,88 +9,31 @@ import {
 import PropTypes from "prop-types";
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
-import ScrapeUrl from "./scrape";
 import ImageUpload from "./image";
 import Ingredients from "./ingredients";
 import Directions from "./directions";
 import Categories from "./categories";
 import Keywords from "./keywords";
 import Input from "../form-inputs/input";
-import Loading from "../loading";
 import { capitalize, titleParse } from "./utils/input-parse";
 import styles from "./recipe-upsert.module.scss";
 
 const RecipeUpsert = props => {
-  // onImageChange(event) {
-  //   console.log(event.target.files[0]);
-  // }
-
-  const { isFetching } = props.recipeData;
-
   const handleKeyDown = e => {
     if (e.target.type !== "textarea" && e.key === "Enter") {
       e.preventDefault();
     }
   };
 
-  const handleChange = e => {
-    console.log(e.target.files[0]);
-    // const {
-    //   input: { onChange }
-    // } = thisprops;
-    //return e.target.files[0];
-  };
-
-  // if (isFetching) {
-  //   return <Loading />;
-  // }
   return (
     <div>
       {props.header}
-      {/*
-        when componentmounts check if clipboard contains a url with
-      navigator.clipboard.readText()
-      change opacity and click if so
-      create action that takes the url with
-
-      function paste() {
-  var pasteText = document.querySelector("#output");
-  pasteText.focus();
-  document.execCommand("paste");
-  console.log(pasteText.textContent);
-}
-
-              -- or --
-
-navigator.clipboard.readText().then(clipText =>
-  document.getElementById("outbox").innerText = clipText)
-
-  in action make axios request to /copy-recipe
-  pass recipe url as param
-
-  make request to the reicpe url and extract the html
-  try the array of querySelectors
-  (https://github.com/MikeBilyeu/RecipeFilter/blob/master/src/js/main.js)
-  convert the html data for recipe to json
-  responed with the recipe in json
-  or responed with couldn't process any recipe data
-
-      use custom input
-      take a string
-      onClick -> action send url to server
-      check url on backend
-      responed with recipe if found
-      dispatch action saying recipe found
-      populate form with recipe data
-      */}
-
       <form
         action="#"
         className={styles.form}
         onKeyDown={handleKeyDown}
         onSubmit={props.handleSubmit(props.onSubmit)}
       >
-        <ScrapeUrl />
         <Field
           name="title"
           className={styles.title}
@@ -101,15 +44,8 @@ navigator.clipboard.readText().then(clipText =>
           parse={titleParse}
         />
 
-        {/*<Field
-          input={{ onChange: handleChange }}
-          name="image"
-          className={styles.imageInput}
-          component={Input}
-          type="file"
-          accept="image/.jpg, image/.png, image/.jpeg"
-        />*/}
-        <ImageUpload />
+        <Field name="image" component={ImageUpload} />
+
         <Ingredients />
         <Directions />
         <Keywords keywords={props.keywords} change={props.change} />
