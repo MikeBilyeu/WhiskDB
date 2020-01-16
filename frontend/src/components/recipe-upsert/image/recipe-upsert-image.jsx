4e13b4c57db1cceccfecaf964305ff819169b0e1
Mike Bilyeu <mikebilyeu512@gmail.com>
2020-01-15 20:06:14 -0500

    Set background image on image input remove separate img tag

--- frontend/src/components/recipe-upsert/image/recipe-upsert-image.jsx
+++ frontend/src/components/recipe-upsert/image/recipe-upsert-image.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { getFormValues } from "redux-form";
 import { connect } from "react-redux";
+import styles from "../recipe-upsert.module.scss";
 
 const ImageUpload = ({ input: { onChange }, ...props }) => {
   const image =
     props.formValues && props.formValues.image ? props.formValues.image : null;
+
+  const backgroundImage = image ? URL.createObjectURL(image) : null;
   return (
-    <div>
-      <label className="imageInput" htmlFor="ImageUpload">
-        <i className="upload icon" />
-        Upload Image
+    <div
+      className={styles.imageInput}
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <label htmlFor="ImageUpload">
+        <span>Upload Image</span>
       </label>
       <input
         type="file"
-        accept="image/.jpg, image/.png, image/.jpeg"
+        accept="image/.jpg;.png;.jpeg;capture=camera"
         onChange={e => {
           console.log(e.target.files[0]);
           onChange(e.target.files[0]);
@@ -21,21 +26,6 @@ const ImageUpload = ({ input: { onChange }, ...props }) => {
         id="ImageUpload"
         style={{ display: "none" }}
       />
-      {image ? (
-        <img
-          alt=""
-          src={URL.createObjectURL(image)}
-          style={{
-            objectFit: "cover",
-            objectPosition: "center",
-            width: "20rem",
-            height: "12rem",
-            borderRadius: ".1rem",
-            display: "block",
-            margin: "1rem auto"
-          }}
-        />
-      ) : null}
     </div>
   );
 };
