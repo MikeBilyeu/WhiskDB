f75a9afb93c0aeaa8834c8fe1c21dcd4a8f76f00
Mike Bilyeu <mikebilyeu512@gmail.com>
2020-01-28 12:38:44 -0500

    File renaming refactor

--- frontend/src/components/app/app.jsx
+++ frontend/src/components/app/app.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import PrivateRoute from "../private-route";
+import PrivateRoute from "../private_route";
 import { checkAuthToken } from "./utils";
 import Nav from "../nav";
 import Home from "../home";
 import Profile from "../profile";
 import Auth from "../auth";
 import Recipe from "../recipe";
-import ScrollUp from "./scroll-up";
+import ScrollUp from "./scroll_up";
 
 checkAuthToken();
 
