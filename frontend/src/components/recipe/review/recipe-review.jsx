6a77a9c3e1dc909f1dbf32abf4411243524a8981
Mike Bilyeu <mikebilyeu512@gmail.com>
2020-03-03 18:42:48 -0500

    Use hooks

--- frontend/src/components/recipe/review/recipe-review.jsx
+++ frontend/src/components/recipe/review/recipe-review.jsx
@@ -1,93 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import classNames from "classnames";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import Stars from "./stars";
 import ReviewDetails from "./details";
-import { toggleReview, submitReview } from "../../../actions/review";
+import { submitReview } from "../../../actions/review";
 import "./recipe-review.scss";
 
-class Review extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      rating: 0,
-      comment: ""
-    };
-  }
+const Review = props => {
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState("");
 
-  componentDidMount() {
-    this.setState({
-      rating: 0,
-      comment: ""
-    });
-  }
-
-  handleChange = e => {
-    this.setState({ comment: e.target.value });
+  const handleChange = e => {
+    setComment(e.target.value);
   };
 
-  handleClick = rating => {
-    this.setState({ rating: rating });
+  const handleClick = rating => {
+    setRating(rating);
   };
 
-  handleSubmit = () => {
+  const handleSubmit = () => {
     const review = {
-      recipe_id: this.props.recipe_id,
-      rating: this.state.rating,
-      comment: this.state.comment
+      recipe_id: props.recipe_id,
+      rating: rating,
+      comment: comment
     };
-    this.props.submitReview(review);
+    props.submitReview(review);
   };
 
-  render() {
-    const { recipe_id, isAuthenticated } = this.props;
-    return (
-      <div className="review">
-        <ReviewDetails recipe_id={recipe_id} />
-        {!isAuthenticated ? (
-          <>
-            <h3 className="review__login-text">
-              You must login to rate a recipe.
-            </h3>
-            <Link to="/auth/login" className="review__login-btn">
-              Login
-            </Link>
-          </>
-        ) : (
-          <>
-            <Stars handleClick={this.handleClick} rating={this.state.rating} />
-            <label className="review__input-label">
-              Review
-              <textarea
-                className="review__input"
-                placeholder="Write a review…"
-                value={this.state.comment}
-                onChange={this.handleChange}
-              />
-            </label>
+  return (
+    <div className="review">
+      <ReviewDetails recipe_id={props.recipe_id} />
+      {!props.isAuthenticated ? (
+        <>
+          <h3 className="review__login-text">
+            You must login to rate a recipe.
+          </h3>
+          <Link to="/auth/login" className="review__login-btn">
+            Login
+          </Link>
+        </>
+      ) : (
+        <>
+          <Stars handleClick={handleClick} rating={rating} />
+          <label className="review__input-label">
+            Review
+            <textarea
+              className="review__input"
+              placeholder="Write a review…"
+              value={comment}
+              onChange={handleChange}
+            />
+          </label>
 
-            <div
-              className={classNames("review__sbmt-btn", {
-                "review__sbmt-btn--active": this.state.rating
-              })}
-              onClick={this.state.rating ? this.handleSubmit : null}
-            >
-              Submit
-            </div>
-          </>
-        )}
-      </div>
-    );
-  }
-}
+          <div
+            className={classNames("review__sbmt-btn", {
+              "review__sbmt-btn--active": rating
+            })}
+            onClick={rating ? handleSubmit : null}
+          >
+            Submit
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 
 const mapStateToProps = state => ({
-  isAuthenticated: state.auth.isAuthenticated,
-  rating: state.recipe.rating
+  isAuthenticated: state.auth.isAuthenticated
 });
 
 export default connect(
   mapStateToProps,
-  { toggleReview, submitReview }
+  { submitReview }
 )(Review);
