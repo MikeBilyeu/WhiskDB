a0ebf9f3a9f187bd62622a47ea37fafdf60d02e2
Mike Bilyeu <mikebilyeu512@gmail.com>
2020-03-04 15:18:40 -0500

    Add new ingredient increments

--- frontend/src/selectors/ingredient-selector/utils/increments.js
+++ frontend/src/selectors/ingredient-selector/utils/increments.js
@@ -7,6 +7,53 @@ const increments = {
     ["⅓", "cup"],
     ["¼", "cup"],
     ["3", "Tbsp"],
+    ["⅛", "cup"],
+    ["1", "Tbsp"],
+    ["¾", "Tbsp"],
+    ["2", "tsp"],
+    ["½", "Tbsp"],
+    ["1", "tsp"],
+    ["¼", "Tbsp"],
+    ["⅔", "tsp"],
+    ["½", "tsp"],
+    ["⅛", "Tbsp"],
+    ["⅓", "tsp"],
+    ["¼", "tsp"],
+    ["⅛", "tsp"]
+  ],
+  metricIncrements: [
+    237, // 1 cup
+    177, // 3/4 cup
+    158, // 2/3 cup
+    118, // 1/2 cup
+    79, // 1/3 cup
+    59, // 1/4 cup
+    45, // 3 Tbsp
+    30, // 1/8 cup
+    15, // 1 Tbsp
+    11, // 3/4 Tbsp
+    10, // 2 tsp
+    7.5, // 1/2 Tbsp
+    5, // 1 tsp
+    3.75, // 1/4 Tbsp
+    3, // 2/3 tsp
+    2.5, // 1/2 tsp
+    2, // 1/8 Tbsp
+    1.5, // 1/3 tsp
+    1.25, // 1/4 tsp
+    0.63 // 1/8 tsp
+  ]
+};
+
+const incrementsOld = {
+  usIncrements: [
+    ["1", "cup"],
+    ["¾", "cup"],
+    ["⅔", "cup"],
+    ["½", "cup"],
+    ["⅓", "cup"],
+    ["¼", "cup"],
+    ["3", "Tbsp"],
     ["2", "Tbsp"],
     ["1", "Tbsp"],
     ["2", "tsp"],
@@ -30,7 +77,7 @@ const increments = {
     10, // 2 tsp
     7.5, // 1/2 Tbsp
     5, // 1 tsp
-    3.75, // 3/4 tsp
+    3.75, // 1/4 Tbsp
     2.5, // 1/2 tsp
     1.25, // 1/4 tsp
     0.63 // 1/8 tsp
