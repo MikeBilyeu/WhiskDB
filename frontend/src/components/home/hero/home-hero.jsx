cb6b8c186f38a0971d354721831565acc6147a9d
Mike Bilyeu <mikebilyeu512@gmail.com>
2020-07-07 14:42:16 -0400

    Change hero image and hero title text to zipiwhisk logo

--- frontend/src/components/home/hero/home-hero.jsx
+++ frontend/src/components/home/hero/home-hero.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import classNames from "classnames";
 import SearchBar from "../../search_bar";
+import Logo from "../../../assets/images/ZipiWhisk-Logo.png";
 import "./home-hero.scss";
 
 const Hero = () => {
   const [smallImgLoaded, setSmallImgLoaded] = useState(false);
   const [bigImgLoaded, setBigImgLoaded] = useState(false);
+
   useEffect(() => {
     // check if the images are loaded
   }, []);
@@ -21,7 +23,7 @@ const Hero = () => {
         <img
           onLoad={() => setSmallImgLoaded(true)}
           alt=""
-          src="https://res.cloudinary.com/mikebilyeuimg/image/upload/c_scale,e_blur:50,q_auto:low,w_15/v1580746441/Recipes/lukas-blazek-f-TWhXOrLiU-unsplash.jpg"
+          src="https://res.cloudinary.com/mikebilyeuimg/image/upload/c_scale,e_blur:50,q_auto:low,w_15/v1594146289/Recipes/joanna-kosinska-Prfs32wh-o4-unsplash.jpg"
           style={{
             width: "1px",
             height: "1px",
@@ -33,7 +35,7 @@ const Hero = () => {
         <img
           onLoad={() => setBigImgLoaded(true)}
           alt=""
-          src="https://res.cloudinary.com/mikebilyeuimg/image/upload/c_scale,h_2500,q_auto:good/v1580746441/Recipes/lukas-blazek-f-TWhXOrLiU-unsplash.jpg"
+          src="https://res.cloudinary.com/mikebilyeuimg/image/upload/c_scale,h_2500,q_auto:good/v1594146289/Recipes/joanna-kosinska-Prfs32wh-o4-unsplash.jpg"
           style={{
             width: "1px",
             height: "1px",
@@ -43,7 +45,7 @@ const Hero = () => {
         />
       </div>
       <div className="hero__container">
-        <h1 className="hero__title">ZipiWhisk</h1>
+        <img className="hero__title" src={Logo} alt="zipiwisk logo" />
         <p className="hero__desc">
           The internet’s source of free recipes.
           <br /> Explore thousands of recipes from around the world!
