6f5ff5dcfc8a4132ddb2e2fa5762e1b98f743ac9
Mike Bilyeu <mikebilyeu512@gmail.com>
2020-07-08 19:11:26 -0400

    Add donation button

--- frontend/src/components/results/no_results/results-no_results.jsx
+++ frontend/src/components/results/no_results/results-no_results.jsx
@@ -1,19 +1,40 @@
 import React from "react";
 import "./results-no_results.scss";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
 
 const NoResults = props => {
-  return (
-    <div className="no-results">
-      <h1 className="no-results__title">
-        Sorry,
-        <br /> we couldn't find any recipes{" "}
-        <span style={{ whiteSpace: "nowrap" }}>:(</span>
-      </h1>
-      <h2 className="no-results__msg">
-        Please try searching for another recipe!
-      </h2>
-    </div>
-  );
+  if (props.location.pathname === "/profile") {
+    return (
+      <div className="no-results">
+        <h1 className="no-results__title">
+          {props.category === "All Categories"
+            ? `You don't have any saved recipes.`
+            : `You don't have any saved ${props.category.toLowerCase()} recipes.`}
+        </h1>
+        <h2 className="no-results__msg">
+          Start saving recipes to see them here!
+        </h2>
+      </div>
+    );
+  } else {
+    return (
+      <div className="no-results">
+        <h1 className="no-results__title">
+          Sorry,
+          <br /> we couldn't find any recipes{" "}
+          <span style={{ whiteSpace: "nowrap" }}>:(</span>
+        </h1>
+        <h2 className="no-results__msg">
+          Please try searching for another recipe!
+        </h2>
+      </div>
+    );
+  }
 };
 
-export default NoResults;
+const mapStateToProps = state => ({
+  category: state.auth.filterRecipes.category
+});
+
+export default withRouter(connect(mapStateToProps)(NoResults));
